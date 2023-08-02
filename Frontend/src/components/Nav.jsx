101343ad34e247c995d6ff3eaf3317a52869e431
송아람 <0807com@naver.com>
2023-08-02 10:47:05 +0900

    main, nav update

--- Frontend/src/components/Nav.jsx
+++ Frontend/src/components/Nav.jsx
@@ -8,14 +8,17 @@ import logo from "../assets/logo.png"; // 로고 이미지 파일의 경로를 �
 const NavLink = styled(Link)`
   text-decoration: none;
   color: black;
-`;
+  margin-right: 10px;
+  
 
+`
+;
 // 컴포넌트를 정의하는 함수
 function CustomNavbar() {
   return (
     <>
       {/* 네비게이션 바 컴포넌트 시작 */}
-      <Navbar style={{ backgroundColor: "#FFF9BE" }} expand="lg">
+      <Navbar style={{ backgroundColor: "#FFF9BE" }} expand="sm">
         {/* 네비게이션 바 브랜드 영역 시작 */}
         <Navbar.Brand href="/">
           <img
@@ -24,7 +27,6 @@ function CustomNavbar() {
             className="d-inline-block align-top" // 부트스트랩 스타일 클래스 설정
             alt="Logo" // 대체 텍스트 설정
           />
-          {/* 로고 이미지의 크기와 alt 속성을 적절히 설정해주세요 */}
         </Navbar.Brand>
         {/* 네비게이션 바 브랜드 영역 끝 */}
         
@@ -32,14 +34,17 @@ function CustomNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         
         {/* 네비게이션 바 메뉴 영역 시작 */}
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto"> {/* ml-auto는 메뉴를 오른쪽으로 정렬 */}
+
+        <Navbar.Collapse id="basic-navbar-nav"> 
+        <div style={{ marginLeft: "auto" }}>
+          <Nav>
             {/* 커스텀 스타일의 링크 컴포넌트들 */}
             <NavLink to="/">홈 </NavLink>
             <NavLink to="/contents"> 컨텐츠 소개 </NavLink>
             <NavLink to="/login">로그인 </NavLink>
             <NavLink to="/signup">회원가입 </NavLink>
           </Nav>
+          </div>
         </Navbar.Collapse>
         {/* 네비게이션 바 메뉴 영역 끝 */}
       </Navbar>
