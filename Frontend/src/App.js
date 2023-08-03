ab81378617b26473d913c329ef787b2305b54586
송아람 <0807com@naver.com>
2023-08-03 20:39:53 +0900

    design: main, nav

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from './components/Nav';
 import './App.css'
 import styled from 'styled-components';
+import PrivateRoute from './components/PrivateRoute';
 
 // page
 import Main from './pages/Main';
@@ -12,8 +13,7 @@ import Album from './pages/Album';
 import Contents from './pages/Contents';
 
 const Wrapper = styled.div`
-  font-family: Pretendard-Regular;
-  font-weight: bold;
+  font-family: Happiness-Sans-Bold;
 `;
 
 function App() {
@@ -25,6 +25,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/contents" element={<Contents />} />
+            {/* <PrivateRoute path="/album" element={<Album />} /> */}
             <Route path="/album" element={<Album />} />
             <Route is path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
