26779118b39bf9f43d3956cc8400e00115542bae
송아람 <0807com@naver.com>
2023-08-10 09:20:13 +0900

    fix: Room, MainVideoComponent

--- Frontend/src/components/MainVideoComponent.jsx
+++ Frontend/src/components/MainVideoComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import userpose from "../utils/userpose";
-import POSE from "../utils/POSE";
+// import POSE from "../utils/POSE";
 import "./UserVideo.css";
 import * as tf from "@tensorflow/tfjs-core"; // 텐서플로우 JS 라이브러리
 
@@ -32,6 +32,8 @@ const handleSkeletonClick = async () => {
       canvasRef.current.width = videoDimensions.width; // 캔버스 너비를 비디오 너비로 설정
       canvasRef.current.height = videoDimensions.height; // 캔버스 높이를 비디오 높이로 설정
       ctx = canvasRef.current.getContext("2d"); // 캔버스에서 2D 렌더링 컨텍스트 가져오기
+      ctx.translate(canvasRef.current.width, 0);
+      ctx.scale(-1, 1);
       userpose.startRender(videoRef.current, ctx); // 사용자 포즈 렌더링 시작 (스켈레톤 그리기)
     } else {
       // 감지기가 로드되어 있지 않을 경우 로딩
@@ -75,8 +77,9 @@ const handleDetectClick = async () => {
 
     return () => {
       // 컴포넌트 언마운트 시 이벤트 리스너 제거
-      // eslint-disable-next-line
-      videoRef.current.removeEventListener("loadedmetadata", handleVideoMetadataLoaded);
+      if (videoRef.current) {
+        videoRef.current.removeEventListener("loadedmetadata", handleVideoMetadataLoaded);
+      }
     };
   });
 
