1b7d51418bf8548c3aec50768bffc30aef23498d
송아람 <0807com@naver.com>
2023-08-12 22:32:03 +0900

    fix: gameSlice, Design: toolbar

--- Frontend/src/components/ToolbarComponent.js
+++ Frontend/src/components/ToolbarComponent.js
@@ -3,6 +3,7 @@ import { Modal } from "react-bootstrap";
 import './Toolbar.css';
 import { switchShowCanvas, switchGameStart } from "../slice/gameSlice";
 import { useSelector, useDispatch } from "react-redux";
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 
 // icone
 import inviteIcon from "../assets/icon/invite.png";
@@ -80,10 +81,47 @@ const ToolbarComponent = (props) => {
     }
   };
   
+  
 return (
   <div className="toolbar-container">
     <div className="iconGroup">
-      { !videoOn ?
+      <OverlayTrigger placement="top" overlay={<Tooltip id="tooltip-video">{!videoOn ? '카메라켜짐' : '카메라꺼짐'}</Tooltip>}>
+        <div onClick={camStatusChanged}>
+          <img src={!videoOn ? videoOnIcon : videoOffIcon} alt={!videoOn ? 'videoOn' : 'videoOff'} className="icon" />
+        </div>
+      </OverlayTrigger>
+      <OverlayTrigger placement="top" overlay={<Tooltip id="tooltip-audio">{!audioOn ? '마이크켜짐' : '마이크꺼짐'}</Tooltip>}>
+        <div onClick={micStatusChanged}>
+          <img src={!audioOn ? audioOnIcon : audioOffIcon} alt={!audioOn ? 'audioOn' : 'audioOff'} className="icon" />
+        </div>
+      </OverlayTrigger>
+      <OverlayTrigger placement="top" overlay={<Tooltip id="tooltip-invite">초대링크</Tooltip>}>
+        <div onClick={handleShow}>
+          <img src={inviteIcon} alt="invite" className="icon" />
+        </div>
+      </OverlayTrigger>
+      <OverlayTrigger placement="top" overlay={<Tooltip id="tooltip-xray">{!showCanvas ? '기본모드' : '엑스레이모드'}</Tooltip>}>
+        <div onClick={clickShowCanvas}>
+          <img src={!showCanvas ? xrayIcon : noXrayIcon} alt={!showCanvas ? 'noXray' : 'xray'} className="icon" />
+        </div>
+      </OverlayTrigger>
+      { !guest &&
+        <OverlayTrigger placement="top" overlay={<Tooltip id="tooltip-gamestart">게임시작</Tooltip>}>
+          <div onClick={gameStatusChanged}>
+            <img src={gamestartIcon} alt="gamestart" className="icon" />
+          </div>
+        </OverlayTrigger> }
+      <OverlayTrigger placement="top" overlay={<Tooltip id="tooltip-leave">방나가기</Tooltip>}>
+        <div onClick={leaveSession}>
+          <img src={leaveIcon} alt="leave" className="icon" />
+        </div>
+      </OverlayTrigger>
+    </div>
+
+    
+   {/* <div className="toolbar-container">
+     <div className="iconGroup">
+       { !videoOn ?
         (<div onClick={camStatusChanged}>
           <img src={videoOnIcon} alt="videoOn" className="icon" />
         </div>) :
@@ -106,16 +144,16 @@ return (
         </div>) :
         (<div onClick={clickShowCanvas}>
           <img src={noXrayIcon} alt="xray" className="icon" />
-        </div>)}
+        </div>)}*/}
       {/* 방장인 경우 게임스타트 */}
-      { !guest ?
+      {/* { !guest ?
         (<div onClick={gameStatusChanged}>
           <img src={gamestartIcon} alt="gamestart" className="icon" />
         </div>) : null }
       <div onClick={leaveSession}>
         <img src={leaveIcon} alt="leave" className="icon" />
       </div>
-    </div>
+    </div>  */}
 
     {/* 초대링크 모달 */}
     <Modal show={show} onHide={handleClose} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
