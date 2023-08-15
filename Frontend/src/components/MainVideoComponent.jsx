d3fd5bfce55e27fd38fe4843ecdf7c766f6fa167
위효선 <wizardisme@naver.com>
2023-08-15 22:56:26 +0900

    update scoremark on MainVideoComponent, gameSlice, Game1 & add gif files to assets

--- Frontend/src/components/MainVideoComponent.jsx
+++ Frontend/src/components/MainVideoComponent.jsx
@@ -5,10 +5,11 @@ import userpose from "../utils/userpose";
 import "./MainVideoComponent.css";
 import * as tf from "@tensorflow/tfjs-core"; // 텐서플로우 JS 라이브러리
 // import axios from "axios";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { handleWebcamRef } from "../slice/gameSlice";
-import { useDispatch } from "react-redux";
 // import NarangNorangIntro from "../assets/game/narangnorang_intro.mp4";
+import superStar from "../assets/game/score/superStar.gif";
+import winner from "../assets/game/score/winner.gif";
 
 // const BASE_URL = 'https://i9c208.p.ssafy.io/api/v1'
 
@@ -19,11 +20,16 @@ const MainVideoComponent = (props) => {
     width: 640,
     height: 480,
   });
-  const dispatch = useDispatch()
-  console.log(videoRef, "videoRef확인")
-  dispatch(handleWebcamRef(videoRef.current))
+  const dispatch = useDispatch();
+  console.log(videoRef, "videoRef확인");
+  dispatch(handleWebcamRef(videoRef.current));
   const showCanvas = useSelector((state) => state.game.showCanvas);
   const detectorRef = useRef(null);
+
+  const nowScore = useSelector((state) => state.game.nowScore);
+  const totalScore = useSelector((state) => state.game.totalScore);
+  const [great, setGreat] = useState(false);
+  const [awesome, setAwesome] = useState(false);
 
   // 비디오 메타데이터 로드 핸들러
   const handleVideoMetadataLoaded = () => {
@@ -94,6 +100,22 @@ const MainVideoComponent = (props) => {
     videoDimensions.height,
   ]);
 
+  useEffect(() => {
+    console.log(nowScore, "동영상 끝 내 점수")
+    if (0.5 <= nowScore && nowScore < 0.7) {
+      setGreat(true);
+      setTimeout(() => {
+        setGreat(false);
+      }, 2000);
+    } else if (nowScore >= 0.7) {
+      setAwesome(true);
+      setTimeout(() => {
+        setAwesome(false);
+      }, 2000);
+    }
+    console.log(great, awesome, "내 상태");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [totalScore]);
 
   // const roomCode = props.streamManager.stream.session.sessionId;
   // const subscriberId = props.streamManager.stream.connection.connectionId;
@@ -119,13 +141,15 @@ const MainVideoComponent = (props) => {
             ref={canvasRef}
             width={videoDimensions.width}
             height={videoDimensions.height}
-            style={{position: "absolute",
-                    top: "0",
-                    left: "0",
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "fill",
-                    zIndex: "2"}}
+            style={{
+              position: "absolute",
+              top: "0",
+              left: "0",
+              width: "100%",
+              height: "100%",
+              objectFit: "fill",
+              zIndex: "2",
+            }}
           ></canvas>
           <video
             autoPlay={true}
@@ -144,13 +168,15 @@ const MainVideoComponent = (props) => {
             ref={canvasRef}
             width={videoDimensions.width}
             height={videoDimensions.height}
-            style={{position: "absolute",
-                    top: "0",
-                    left: "0",
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "fill",
-                    zIndex: "2"}}
+            style={{
+              position: "absolute",
+              top: "0",
+              left: "0",
+              width: "100%",
+              height: "100%",
+              objectFit: "fill",
+              zIndex: "2",
+            }}
           ></canvas>
           <video
             autoPlay={true}
@@ -158,6 +184,34 @@ const MainVideoComponent = (props) => {
             width={videoDimensions.width}
             height={videoDimensions.height}
           />
+          {great ? (
+            <img
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "50%",
+                height: "50%",
+                zIndex: "1",
+              }}
+              src={superStar}
+              alt="great"
+            />
+          ) : null}
+          {awesome ? (
+            <img
+              style={{
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                width: "50%",
+                height: "50%",
+                zIndex: "1",
+              }}
+              src={winner}
+              alt="awesome"
+            />
+          ) : null}
         </div>
       ) : null}
     </div>
